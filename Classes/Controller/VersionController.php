ac191ea358dbe59f16d4d0363fe05b6a07347cae
pottmar <p.ottmar@12bis3.de>
2022-09-28 12:59:44 +0200

    #7199: element changes

---
+++ Classes/Controller/VersionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Code711\Code711Api\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Http\JsonResponse;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class VersionController extends ActionController
+{
+
+    public function getVersionAction(ServerRequestInterface $request): ResponseInterface
+    {
+        $version = [
+            'version' => VersionNumberUtility::getNumericTypo3Version(),
+        ];
+        return new JsonResponse($version);
+    }
+}
