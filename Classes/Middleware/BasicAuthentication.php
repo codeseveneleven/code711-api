f0ecd5b57be25e2b3c66860c32ef7d75169e2aaf
pottmar <p.ottmar@12bis3.de>
2022-09-29 12:25:43 +0200

    add auth api

---
+++ Classes/Middleware/BasicAuthentication.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Code711\Code711Api\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use TYPO3\CMS\Core\Http\JsonResponse;
+
+class BasicAuthentication implements MiddlewareInterface
+{
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $authorization = $request->getHeader('authorization')[0];
+        if (strpos($authorization, 'Basic ') !== 0) {
+            return new JsonResponse(
+                [
+                    'message' => 'Authorization header not found.',
+                    'headers' => $request->getHeaders(),
+                ],
+                403
+            );
+        }
+        $authKey = trim(str_replace('Basic ', '', $authorization), '"\'');
+        $key = base64_encode(getenv('REST_API_USER') . ':' . getenv('REST_API_PW'));
+        if ($authKey !== $key) {
+            return new JsonResponse(['message' => 'Web Token is invalid'], 401);
+        }
+        return $handler->handle($request);
+    }
+
+}
