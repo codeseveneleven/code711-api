1eae180ecefec75f455717f4d15275e64a9ce3c2
pottmar <p.ottmar@12bis3.de>
2022-12-19 14:44:25 +0100

    fix key

--- Classes/Middleware/BasicAuthentication.php
+++ Classes/Middleware/BasicAuthentication.php
@@ -13,21 +13,24 @@ class BasicAuthentication implements MiddlewareInterface
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $authorization = $request->getHeader('authorization')[0];
+        $header = $request->getHeader('authorization');
+        if (isset($header[0])) {
+            $authorization = $request->getHeader('authorization')[0];
 
-        if (strpos($authorization, 'Basic ') !== 0) {
-            return new JsonResponse(
-                [
-                    'message' => 'Authorization header not found.',
-                    'headers' => $request->getHeaders(),
-                ],
-                403
-            );
-        }
-        $authKey = trim(str_replace('Basic ', '', $authorization), '"\'');
-        $key = base64_encode(getenv('REST_API_USER') . ':' . getenv('REST_API_PW'));
-        if ($authKey !== $key) {
-            return new JsonResponse(['message' => 'Web Token is invalid'], 401);
+            if (strpos($authorization, 'Basic ') !== 0) {
+                return new JsonResponse(
+                    [
+                        'message' => 'Authorization header not found.',
+                        'headers' => $request->getHeaders(),
+                    ],
+                    403
+                );
+            }
+            $authKey = trim(str_replace('Basic ', '', $authorization), '"\'');
+            $key = base64_encode(getenv('REST_API_USER') . ':' . getenv('REST_API_PW'));
+            if ($authKey !== $key) {
+                return new JsonResponse(['message' => 'Web Token is invalid'], 401);
+            }
         }
         return $handler->handle($request);
     }
